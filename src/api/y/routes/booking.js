e8d1d606627b32a635bcf6ef678275e01aebcb3d
prithviraj m pillai <prithvi_mp@yahoo.in>
2024-09-21 18:17:24 +0530

    first push

---
+++ src/api/y/routes/booking.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * y router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::y.booking');
---
+++ src/api/y/services/booking.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * y service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::y.booking');
